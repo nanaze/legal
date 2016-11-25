ca62b79f23ea722ad9b64196ed33769873469c82
Nathan Naze <nanaze@gmail.com>
2016-11-25 12:21:31 -0800

    add appengine_config script to properly fill path

---
+++ appengine_config.py
@@ -0,0 +1,11 @@
+import os
+import site
+
+app_path = os.path.dirname(__file__)
+third_party_path = os.path.join(app_path, 'third_party')
+
+# Add path for Google Cloud Storage client
+gcs_client_path = os.path.join(third_party_path, 
+                               'appengine-gcs-client/python/src/')
+
+site.addsitedir(gcs_client_path)
