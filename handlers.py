d4ffb87485bccc8e62c6e26be8011ca6cb062e68
Nathan Naze <nanaze@gmail.com>
2016-11-26 15:26:30 -0800

    Migrate menu over to use template generation and start generating the template dict.

--- handlers.py
+++ handlers.py
@@ -4,22 +4,33 @@ import config
 import webapp2
 import logging
 import formmail
-
+import collections
 
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
 
+NavigationEntry = collections.namedtuple('NavigationEntry', ['href', 'text'])
 
+def _GetNavigationEntries():
+  return [
+    NavigationEntry('/', 'What we do.'),
+    NavigationEntry('/resources', 'Resources.'),
+    NavigationEntry('/contact', 'Contact us.')
+  ]
 
+def _GetTemplateDict():
+  return {
+    'navigation': _GetNavigationEntries()
+  }
 
 class MainPage(webapp2.RequestHandler):
   def get(self):
     self.response.headers['Content-Type'] = 'text/html'
 
     template = JINJA_ENVIRONMENT.get_template('templates/index.html')
-    content = template.render({})
+    content = template.render(_GetTemplateDict())
     self.response.write(content)
 
 class About(webapp2.RequestHandler):
@@ -27,7 +38,7 @@ class About(webapp2.RequestHandler):
     self.response.headers['Content-Type'] = 'text/html'
 
     template = JINJA_ENVIRONMENT.get_template('templates/about.html')
-    content = template.render({})
+    content = template.render(_GetTemplateDict())
     self.response.write(content)
 
 class Form(webapp2.RequestHandler):
@@ -35,7 +46,7 @@ class Form(webapp2.RequestHandler):
     self.response.headers['Content-Type'] = 'text/html'
 
     template = JINJA_ENVIRONMENT.get_template('templates/form.html')
-    content = template.render({})
+    content = template.render(_GetTemplateDict())
     self.response.write(content)
 
   def post(self):
