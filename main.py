7ac7f234f8273ce7200fb0e7f1337b823828354c
Nathan Naze <nanaze@gmail.com>
2016-11-27 12:55:34 -0800

    run in debug mode for dev server only

--- main.py
+++ main.py
@@ -6,9 +6,13 @@ import webapp2
 
 logging.basicConfig(level=logging.INFO)
 
+server = os.environ['SERVER_SOFTWARE']
+
+is_debug = 'Develop' in server
+
 app = webapp2.WSGIApplication([
   ('/', handlers.MainPage),
   ('/resources', handlers.Resources),
   ('/contact', handlers.Contact),
   ('/form', handlers.Form),
-], debug=True)
+], debug=is_debug)
