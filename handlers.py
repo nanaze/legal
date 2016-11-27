d9a8318d4239bcb79c5c37a635f7b80a20a26c6a
Nathan Naze <nanaze@gmail.com>
2016-11-27 12:23:32 -0800

    add trim_blocks

--- handlers.py
+++ handlers.py
@@ -9,6 +9,7 @@ import collections
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
     extensions=['jinja2.ext.autoescape'],
+    trim_blocks=True,
     autoescape=True)
 
 NavigationEntry = collections.namedtuple('NavigationEntry', ['href', 'text'])
