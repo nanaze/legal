ce0b0de2175245ee2986800a66b6f61909a7385a
Nathan Naze <nanaze@gmail.com>
2016-12-04 16:44:44 -0800

    Modify templates to get state data from a data file.

--- contact.py
+++ contact.py
@@ -1,11 +1,24 @@
+import csv
 import handlers
 import webapp2
 
+def _GetStateCodes():
+  with open('data/states.txt') as f:
+    for line in f:
+      line = line.strip()
+      if line:
+        yield line
+
+STATE_CODES = sorted(list(_GetStateCodes()))
+      
 class Contact(webapp2.RequestHandler):
   def get(self):
     self.response.headers['Content-Type'] = 'text/html'
 
     template = handlers.JINJA_ENVIRONMENT.get_template('form.html')
     template_dict = handlers.GetTemplateDict(self.request, additional_stylesheets=['form.css'])
+
+    template_dict['states'] = [''] + STATE_CODES
+    
     content = template.render(template_dict)
     self.response.write(content)
