fb222fc2a31ca1731f80305c4382531534a5417f
David Altenburg <david@inventables.com>
2017-01-30 19:51:35 -0600

    Modify contact form to load areas of need from a separate file

--- contact.py
+++ contact.py
@@ -9,8 +9,18 @@ def _GetStateCodes():
       if line:
         yield line
 
+def _GetAreasOfNeed():
+  with open('data/areas_of_need.txt') as f:
+    for line in f:
+      line = line.strip()
+      if line:
+        yield line
+
+
 STATE_CODES = sorted(list(_GetStateCodes()))
-      
+
+AREAS_OF_NEED = list(_GetAreasOfNeed())
+
 class Contact(webapp2.RequestHandler):
   def get(self):
     self.response.headers['Content-Type'] = 'text/html'
@@ -19,13 +29,14 @@ class Contact(webapp2.RequestHandler):
       'form.css',
       'columns.css'
     ]
-    
+
     template = handlers.JINJA_ENVIRONMENT.get_template('contact.html')
     template_dict = handlers.GetTemplateDict(
       self.request,
       additional_stylesheets=additional_stylesheets)
 
     template_dict['states'] = [''] + STATE_CODES
-    
+    template_dict['areas_of_need'] = AREAS_OF_NEED
+
     content = template.render(template_dict)
     self.response.write(content)
